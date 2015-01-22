ed311d99e74d0e393c511c12c7e6f601861058b6
ungsikyun <ungsik.yun@gmail.com>
2015-01-22 18:27:58 +0900

    Modi - add some Item class for scraping and example addresses

--- oneplusone/oneplusone/items.py
+++ oneplusone/oneplusone/items.py
@@ -12,3 +12,41 @@ class OneplusoneItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class ForumUser(scrapy.Item):
+	'''
+	example)
+	https://forums.oneplus.net/members/karan-kadakia.481070/
+	'''
+	pass
+
+class ForumThread(scrapy.Item):
+	'''
+	example)
+	https://forums.oneplus.net/threads/faq-frequently-asked-questions-including-carrier-compatibility.7195/
+	https://forums.oneplus.net/threads/faq-frequently-asked-questions-including-carrier-compatibility.7195/page-2
+	'''
+	pass
+
+class TheOneForum(scrapy.Item):
+	pass
+
+
+
+
+'''
+https://forums.oneplus.net/forums/announcements/
+https://forums.oneplus.net/forums/contests/
+
+https://forums.oneplus.net/forums/the-one/
+https://forums.oneplus.net/forums/cyanogenmod-11s/
+https://forums.oneplus.net/forums/dev-roms/
+https://forums.oneplus.net/forums/accessories/
+https://forums.oneplus.net/forums/invites/
+
+https://forums.oneplus.net/forums/off-topic/
+https://forums.oneplus.net/forums/introduce-yourself/
+https://forums.oneplus.net/forums/tech-talk/
+https://forums.oneplus.net/forums/fan-gatherings/
+
+'''
