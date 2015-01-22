2e4bcd93fb93e4853a6fc504c80ca3778cb7d2f4
ungsikyun <ungsik.yun@gmail.com>
2015-01-22 12:56:09 +0900

    Add venv, scrapy project

---
+++ oneplusone/oneplusone/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for oneplusone project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'oneplusone'
+
+SPIDER_MODULES = ['oneplusone.spiders']
+NEWSPIDER_MODULE = 'oneplusone.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'oneplusone (+http://www.yourdomain.com)'
