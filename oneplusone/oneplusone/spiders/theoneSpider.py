167da2f8720700df22065ef5901ecad3dba900ed
Ungsik Yun <ungsik.yun@gmail.com>
2015-01-24 16:42:38 +0900

    Modi - Crawl Spider 사용하게 변경.

--- oneplusone/oneplusone/spiders/theoneSpider.py
+++ oneplusone/oneplusone/spiders/theoneSpider.py
@@ -2,30 +2,37 @@
 
 __author__ = 'ungsik'
 from scrapy import Spider
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 from oneplusone.items import ForumThread
 
-class TheoneSpider(Spider):
+class TheoneSpider(CrawlSpider):
 
     name = "theOne"
 
-    allowed_domains = ["https://forums.oneplus.net/"]
+    allowed_domains = ["forums.oneplus.net"]
 
     start_urls = [
-        "https://forums.oneplus.net/forums/announcements/",
-        "https://forums.oneplus.net/forums/contests/",
+        # "https://forums.oneplus.net/forums/announcements/",
+        # "https://forums.oneplus.net/forums/contests/",
         "https://forums.oneplus.net/forums/the-one/",
-        "https://forums.oneplus.net/forums/cyanogenmod-11s/",
-        "https://forums.oneplus.net/forums/dev-roms/",
-        "https://forums.oneplus.net/forums/accessories/",
-        "https://forums.oneplus.net/forums/invites/",
-        "https://forums.oneplus.net/forums/off-topic/",
-        "https://forums.oneplus.net/forums/introduce-yourself/",
-        "https://forums.oneplus.net/forums/tech-talk/",
-        "https://forums.oneplus.net/forums/fan-gatherings/",
+        # "https://forums.oneplus.net/forums/cyanogenmod-11s/",
+        # "https://forums.oneplus.net/forums/dev-roms/",
+        # "https://forums.oneplus.net/forums/accessories/",
+        # "https://forums.oneplus.net/forums/invites/",
+        # "https://forums.oneplus.net/forums/off-topic/",
+        # "https://forums.oneplus.net/forums/introduce-yourself/",
+        # "https://forums.oneplus.net/forums/tech-talk/",
+        # "https://forums.oneplus.net/forums/fan-gatherings/",
     ]
 
-    def parse(self, response):
+    rules = (
+        Rule(LinkExtractor(allow=('threads\/.+', )), callback='parse_thread', follow=True),
+    )
 
+
+    @classmethod
+    def parse_forum(response):
         threads_list = response.xpath('//div[@class="titleText"]')
         for thread in threads_list:
             item = ForumThread()
@@ -45,3 +52,15 @@ class TheoneSpider(Spider):
 
 
             print item
+        return item
+
+    def parse_thread(self, response):
+        item = ForumThread()
+        item['forum'] = response.xpath('//*[@id="top"]/div/div/div[1]/nav/fieldset/span/span[3]/a/span/text()').extract()
+        item['title'] = response.xpath('//*[@id="top"]/div/div/div[2]/h1/text()').extract()
+        item['url'] = response.url
+        # item['date'] = thread.xpath('.//abbr[@class="DateTime"]/text()').extract()
+
+        print item
+        return item
+
